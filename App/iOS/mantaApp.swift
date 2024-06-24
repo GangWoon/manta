42d6aecb451f6457e540c34a5d1592ef3a319d84
GangWoon <gangwoon2018@gmail.com>
2024-06-24 22:14:07 +0900

    Init project

---
+++ App/iOS/mantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mantaApp.swift
+//  manta
+//
+//  Created by Cloud on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct mantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+  name: "manta",
+  platforms: [.iOS(.v16)],
+  products: [
+    .library(
+      name: "NewAndNowFeature",
+      targets: ["NewAndNowFeature"]),
+  ],
+  targets: [
+    .target(
+      name: "NewAndNowFeature"
+    ),
+  ]
+)
