deaab71df3df29f8a562505c7a00bce1c050d47a
GangWoon <gangwoon2018@gmail.com>
2024-06-28 23:52:43 +0900

    Fix to build

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
       targets: ["NewAndNowFeature"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "main"),
-    .package(url: "https://github.com/pointfreeco/swift-dependencies", branch: "main")
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.11.2"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.3.1")
   ],
   targets: [
     .target(
