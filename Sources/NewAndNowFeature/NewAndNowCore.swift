680abef72ebcc553eda3cc975909b82c1592c5e5
GangWoon <gangwoon2018@gmail.com>
2024-07-04 13:57:56 +0900

    Fix NewAndNowCore

--- Sources/NewAndNowFeature/NewAndNowCore.swift
+++ Sources/NewAndNowFeature/NewAndNowCore.swift
@@ -8,22 +8,41 @@ public struct NewAndNowCore {
   public struct State: Equatable, Sendable {
     public var selectedReleaseStatus: WebToonCore.State.ReleaseStatus
     public var scrollCategoryList: [WebToonCore.State.ReleaseStatus]
+    
+    /// 복잡한 스크롤 뷰 로직은 뷰에서만 처리하려고 설계했지만, 스크롤 뷰 해더를 강제적으로 노출시키기 위해서 만든 값입니다.
+    /// 해더를 노출시키기는 로직을 리듀서 내부에서 관리하시며 안됩니다.
+    public var forceShowingHeader: Bool
+    
+    public var notificationItemList: [NotificationItem] {
+      webToonList
+        .filter(\.isNotified)
+        .compactMap(\.notificationItem)
+    }
+    public struct NotificationItem: Equatable, Sendable, Identifiable {
+      public var id: WebToonCore.State.ID
+      public var thumbnail: URL?
+      public var releaseDate: Date
+    }
+    
     public var webToonList: IdentifiedArrayOf<WebToonCore.State>
     
     public init(
       selectedReleaseStatus: WebToonCore.State.ReleaseStatus = .comingSoon,
       scrollCategoryList: [WebToonCore.State.ReleaseStatus] = WebToonCore.State.ReleaseStatus.allCases,
+      forceShowingHeader: Bool = false,
       webToonList: IdentifiedArrayOf<WebToonCore.State> = []
     ) {
       self.selectedReleaseStatus = selectedReleaseStatus
       self.scrollCategoryList = scrollCategoryList
+      self.forceShowingHeader = forceShowingHeader
       self.webToonList = webToonList
     }
     
     func scrollID(for releaseStatus: WebToonCore.State.ReleaseStatus) -> WebToonCore.State.ID? {
       webToonList
         .filter { $0.releaseStatus == releaseStatus }
-        .first?.id
+        .first?
+        .id
     }
   }
   
@@ -55,7 +74,9 @@ public struct NewAndNowCore {
             case .undocumented(statusCode: let code):
               print(code)
             }
-          } catch { }
+          } catch { 
+            print(error)
+          }
         }
         
       case .fetchResponse(let data):
@@ -67,12 +88,16 @@ public struct NewAndNowCore {
         return .none
         
       case .webToonList(.element(id: let id, action: let action)):
-        if 
-          case .onAppear = action,
-          let selectedReleaseStatus = state.webToonList[id: id]?.releaseStatus,
-          state.selectedReleaseStatus != selectedReleaseStatus
-        {
-          state.selectedReleaseStatus = selectedReleaseStatus
+        guard
+          let webToonState = state.webToonList[id: id]
+        else { return .none }
+        switch action {
+        case .onAppear:
+          if state.selectedReleaseStatus != webToonState.releaseStatus {
+          state.selectedReleaseStatus = webToonState.releaseStatus
+        }
+        case .notifyButtonTapped:
+          state.forceShowingHeader = true
         }
         return .none
         
@@ -83,7 +108,19 @@ public struct NewAndNowCore {
     .forEach(\.webToonList, action: \.webToonList) {
       WebToonCore()
     }
-    
+  }
+}
+
+extension WebToonCore.State {
+  var notificationItem: NewAndNowCore.State.NotificationItem? {
+    if let releaseDate {
+      return .init(
+        id: id,
+        thumbnail: thumbnailURL,
+        releaseDate: releaseDate
+      )
+    }
+    return nil
   }
 }
 
@@ -91,7 +128,7 @@ private extension Components.Schemas.NewAndNow.WebToon {
   func webToonState(_ id: UUID, releaseStatus: WebToonCore.State.ReleaseStatus) -> WebToonCore.State {
     .init(
       id: id,
-      releaseStatus: releaseStatus,
+      releaseDate: releaseDate,
       title: title,
       tags: tags,
       thumbnailURL: thumbnail,
