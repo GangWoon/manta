95e27e904ef2ff9d191487f731a7e6b0becd7bf6
GangWoon <gangwoon2018@gmail.com>
2024-07-12 23:20:56 +0900

    Add Shape style

--- Sources/ViewHelper/Shape+Extension.swift
+++ Sources/ViewHelper/Shape+Extension.swift
@@ -12,3 +12,31 @@ extension Shape {
     }
   }
 }
+
+extension ShapeStyle where Self == AngularGradient {
+  public static var palette: AngularGradient {
+    AngularGradient(
+      gradient: Gradient(stops: [
+        .init(color: .blue, location: 0.0),
+        .init(color: .purple, location: 0.2),
+        .init(color: .red, location: 0.4),
+        .init(color: .mint, location: 0.5),
+        .init(color: .indigo, location: 0.7),
+        .init(color: .pink, location: 0.9),
+        .init(color: .blue, location: 1.0),
+      ]),
+      center: .center,
+      startAngle: Angle(radians: .zero),
+      endAngle: Angle(radians: .pi * 2)
+    )
+  }
+
+  public static var transparent: AngularGradient {
+    AngularGradient(
+      gradient: Gradient(stops: []),
+      center: .center,
+      startAngle: Angle(radians: .zero),
+      endAngle: Angle(radians: .pi * 2)
+    )
+  }
+}
