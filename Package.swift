f0cd7ff8eebb2abd54c0036582c76ea4d2b11881
GangWoon <gangwoon2018@gmail.com>
2024-07-17 21:02:01 +0900

    Extract LayImage to ViewHelper Module

--- Package.swift
+++ Package.swift
@@ -6,12 +6,21 @@ let package = Package(
   platforms: [.iOS(.v16)],
   products: [
     .library(
-      name: "NewAndNowFeature",
-      targets: ["NewAndNowFeature"]),
+      name: "AppFeature",
+      targets: [
+        "NewAndNowFeature",
+        "WebtoonDetailFeature"
+      ]
+    ),
+    .library(
+      name: "WebtoonDetail",
+      targets: ["WebtoonDetailFeature"]
+    ),
   ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.11.2"),
     .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.3.1"),
+    .package(url: "https://github.com/pointfreeco/swift-perception.git", from: "1.3.2")
   ],
   targets: [
     .target(
@@ -19,9 +28,17 @@ let package = Package(
       dependencies: [
         "ApiClient",
         .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-        "SharedModels"
+        "WebtoonDetailFeature",
+        "SharedModels",
+        "ViewHelper"
+      ]
+    ),
+    .target(
+      name: "WebtoonDetailFeature",
+      dependencies: [
         .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-        "SharedModels"
+        "SharedModels",
+        "ViewHelper"
       ]
     ),
     .target(
@@ -33,7 +50,12 @@ let package = Package(
       ],
       resources: [.process("Resources")]
     ),
-    .target(name: "ViewHelper"),
+    .target(
+      name: "ViewHelper",
+      dependencies: [
+        .product(name: "Perception", package: "swift-perception")
+      ]
+    ),
     .target(name: "SharedModels")
   ]
 )
