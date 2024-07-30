38e030c439b9428c792ebcb09496d53e6c4dd5cb
GangWoon <gangwoon2018@gmail.com>
2024-07-30 20:33:54 +0900

    Add BadgeStyle ViewModifier to standardize badge appearance

---
+++ Sources/WebtoonDetailFeature/BadgeStyle.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct BadgeStyle: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.caption2).bold()
+      .foregroundStyle(.manta.white)
+      .padding(.vertical, 1)
+      .padding(.horizontal, 2)
+      .background {
+        RoundedRectangle(cornerRadius: 4)
+          .fill(.manta.gray)
+      }
+  }
+}
+
+extension View {
+  func badge() -> some View {
+    modifier(BadgeStyle())
+  }
+}
