1f07b1eb10889f4c028ffd50d62f19ebe4e08cd6
GangWoon <gangwoon2018@gmail.com>
2024-08-03 01:15:44 +0900

    Fix userNotificationCenter(_:didReceive:) leak issue

--- Sources/AppFeature/AppView.swift
+++ Sources/AppFeature/AppView.swift
@@ -34,6 +34,18 @@ public struct AppReducer {
     Scope(state: \.newAndNow, action: \.newAndNow) {
       NewAndNowCore()
     }
+    
+    Reduce<State, Action> { state, action in
+      switch action {
+      case .appDelegate(.userNotifications(.didReceiveResponse(_, completionHandler: let completionHandler))):
+        // TODO: - Notification에서 데이터 전달 받아서 다른 작업 처리하기
+        return .run { _ in
+          completionHandler()
+        }
+      default:
+        return .none
+      }
+    }
   }
 }
 
